9d5c714c58f5e1827af8d5743b451fac4ee5d987
ernesto2011 <skillmx107@gmail.com>
2024-09-04 23:07:31 -0600

    :rocket: configuración inicial de express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+app.listen(5000,()=>{
+    console.log('Server is running on port 5000');
+})
+
+app.use(express.json());
+
