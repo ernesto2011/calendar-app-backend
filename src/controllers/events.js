437f7f260456339193bae37c1e3f694ca4f0930e
ernesto2011 <skillmx107@gmail.com>
2024-12-09 21:11:15 -0600

    crud events

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,33 +1,76 @@
+import { Event } from "../models/Event.js";
 
-export const getEvents = (req, res)=>{
-    res.json({
-        ok: true,
-        msg: 'getEvents'
-    })
-}
-export const getEvent = (req, res)=>{
-    res.json({
-        ok: true,
-        msg: 'createEvent'
-    })
-}
-export const createEvent = (req, res)=>{
-    const {title, start, end} = req.body
-    
-    res.json({
-        ok: true,
-        msg: 'createEvent'
-    })
-}
-export const updateEvent = (req, res)=>{
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    })
-}
-export const deleteEvent = (req, res)=>{
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
-}
+export const getEvents = async(req, res) => {
+  try {
+    const uid = req.uid;
+    const events = await Event.find({'user':uid}).populate('user', 'name');
+    res.status(200).json({
+      ok: true,
+      events,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Ha ocurrido un error!!!",
+    });
+  }
+};
+export const getEvent = (req, res) => {
+  res.json({
+    ok: true,
+    msg: "createEvent",
+  });
+};
+export const createEvent = async (req, res) => {
+  const event = new Event(req.body);
+  try {
+    event.user = req.uid;
+    const savedEvent = await event.save();
+    res.status(200).json({
+      ok: true,
+      event: savedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Ha ocurrido un error!!!",
+    });
+  }
+};
+export const updateEvent = async(req, res) => {
+    const eventId = req.params.id
+    try {
+        const event = await Event.findById(eventId);
+        if(!event){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe con ese id'
+            })
+        }
+        if(event.user.toString() !== req.uid){
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            })
+        }
+        const newEvent ={
+            ...req.body,
+            user: req.uid
+        }
+        const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, {new: true})
+        res.status(200).json({
+            ok: true,
+            eventUpdated
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Ha ocurrido un error'
+        })
+    }
+};
+export const deleteEvent = (req, res) => {
+};
